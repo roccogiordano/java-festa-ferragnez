c3b95d710720eb2df023e53a878d43dcf3f70a4b
roccogiordano <rocco.giordano573@gmail.com>
2024-12-01 10:51:28 +0100

    feat: added user input

--- java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -1,13 +1,27 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
 
         // Array Declaration
 
-        String[] Guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli"," J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        String[] Guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
         //
 
+
+        // User Input
+
+        Scanner Input = new Scanner(System.in);
+
+        System.out.println("Enter your name...");
+
+        String UserName = Input.nextLine();
+
+        Input.close();
+
+        //
     }
 }
