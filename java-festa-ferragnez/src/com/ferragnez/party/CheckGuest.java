62cde794844d3e657c0f794152ef7cf5b58c6305
roccogiordano <rocco.giordano573@gmail.com>
2024-12-01 10:31:00 +0100

    feat: array declaration

---
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,13 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+
+        // Array Declaration
+
+        String[] Guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli"," J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        //
+
+    }
+}
